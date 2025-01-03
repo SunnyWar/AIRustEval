fc2542d17c997816f422a21e9c62984cede3216b
SunnyWar <4521425+SunnyWar@users.noreply.github.com>
2025-01-02 17:56:55 -0700

    manually fixed syntha.ai function

--- src/module3/mod.rs
+++ src/module3/mod.rs
@@ -5,27 +5,28 @@ pub fn name() -> &'static str {
 #[inline(never)]
 pub fn levenshtein_distance(s: &str, t: &str) -> usize {
     let (m, n) = (s.len(), t.len());
-    let mut dp = vec![0; n + 1];
+    let mut dp = vec![vec![0; n + 1]; m + 1];
 
     for i in 0..=m {
-        let mut prev = i;
         for j in 0..=n {
             if i == 0 {
-                dp[j] = j;
+                dp[i][j] = j;
             } else if j == 0 {
-                prev = i;
+                dp[i][j] = i;
             } else {
                 let cost = if s.chars().nth(i - 1) == t.chars().nth(j - 1) {
                     0
                 } else {
                     1
                 };
-                let temp = dp[j];
-                dp[j] = *[prev + cost, dp[j - 1] + 1, dp[j] + 1].iter().min().unwrap();
-                prev = temp;
+                dp[i][j] = *[
+                    dp[i - 1][j] + 1,      // Deletion
+                    dp[i][j - 1] + 1,      // Insertion
+                    dp[i - 1][j - 1] + cost, // Substitution
+                ].iter().min().unwrap();
             }
         }
     }
 
-    dp[n]
+    dp[m][n]
 }
